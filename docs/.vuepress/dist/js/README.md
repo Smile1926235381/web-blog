0aadb0bab1d5473df363563e221d49792ea3ed1e
赵丕友 <17852335869@163.com>
2023-06-01 14:00:22 +0800

    1.网址修改

--- docs/.vuepress/dist/js/README.md
+++
@@ -1,6 +0,0 @@
-vuepress-plugin-pet网页宠物插件
-在config.js加以下进行使用
-   head: [
-        [ 'script', { src: "/js/pet.js" }],
-        ['script', { src: "/js/base.js" }]
-    ],
---
+++ docs/data/guide.js
@@ -0,0 +1,535 @@
+export const guideObj = {
+  // 搜索引擎
+  cardData0: [
+    {
+      id: "0",
+      cardSrc: "http://www.baidu.com/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220104224044.png",
+      cardName: "百度",
+      cardContent:
+        "百度——全球最大的中文搜索引擎及最大的中文网站，全球领先的人工智能公司",
+    },
+    {
+      cardSrc: "http://www.google.com/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220104225539.png",
+      cardName: "Google",
+      cardContent: "全球最大的搜索引擎公司",
+    },
+    {
+      cardSrc: "https://www.bing.com/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220104224430.png",
+      cardName: "Bing",
+      cardContent: "微软公司推出的用以取代Live Search的搜索引擎",
+    },
+  ],
+  // 前端框架
+  cardData1: [
+    {
+      cardSrc: "https://cn.vuejs.org/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220105001047.png",
+      cardName: "Vue",
+      cardContent: "渐进式 JavaScript 框架",
+    },
+    {
+      cardSrc: "https://react.docschina.org/",
+      cardImgSrc:
+        "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      cardName: "React",
+      cardContent: "构建用户界面的 JavaScript 库",
+    },
+
+    {
+      cardSrc: "https://www.tslang.cn/index.html",
+      cardImgSrc: "https://cdn.docschina.org/home/logo/typescript.svg",
+      cardName: "TypeScript",
+      cardContent: "JavaScript的超集",
+    },
+    {
+      cardSrc: "http://nodejs.cn/learn",
+      cardImgSrc: "https://cdn.docschina.org/home/logo/node.svg",
+      cardName: "Node",
+      cardContent: "node使用教程",
+    },
+  ],
+  // 编程教程
+  cardData2: [
+    {
+      cardSrc: "https://element.eleme.cn/#/zh-CN/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220105001602.png",
+      cardName: "Element-UI",
+      cardContent:
+        "Element，一套为开发者、设计师和产品经理准备的基于 Vue 的桌面端组件库",
+    },
+    {
+      cardSrc: "https://element-plus.gitee.io/zh-CN/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220105001602.png",
+      cardName: "Element-PLUS",
+      cardContent: "基于 Vue 3，面向设计师和开发者的组件库",
+    },
+    {
+      cardSrc: "https://next.antdv.com/docs/vue/introduce-cn/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220105223748.svg",
+      cardName: "Ant Design Vue",
+      cardContent: "Vue UI 之 Ant Design Vue，蚂蚁金服的 Vue 框架",
+    },
+    {
+      cardSrc: "https://ant.design/components/overview-cn/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220105223748.svg",
+      cardName: "Antd",
+      cardContent:
+        "助力设计开发者「更灵活」地搭建出「更美」的产品，让用户「快乐工作」～",
+    },
+    {
+      cardSrc: "https://www.iviewui.com/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220105001656.png",
+      cardName: "View UI",
+      cardContent: "View UI 是一套基于 Vue.js 的高质量UI 组件库",
+    },
+    {
+      cardSrc: "https://v1.uviewui.com/",
+      cardImgSrc: "https://v1.uviewui.com/common/logo.png",
+      cardName: "uView UI",
+      cardContent:
+        "uView UI，是uni-app生态最优秀的UI框架，全面的组件和便捷的工具会让您信手拈来，如鱼得水",
+    },
+    {
+      cardSrc: "http://vant-contrib.gitee.io/vant/#/zh-CN",
+      cardImgSrc: "	https://fastly.jsdelivr.net/npm/@vant/assets/logo.png",
+      cardName: "Vant",
+      cardContent: "轻量、可定制的移动端 Vue 组件库",
+    },
+    {
+      cardSrc: "https://taro-ui.jd.com/#/",
+      cardImgSrc: "https://taro-ui.jd.com/img/logo-taro.png",
+      cardName: "Taro UI",
+      cardContent: "一套基于 Taro 框架开发的多端 UI 组件库",
+    },
+  ],
+  // 跨端平台
+  cardData3: [
+    {
+      id: "3",
+      cardSrc: "https://uniapp.dcloud.net.cn/",
+      cardImgSrc:
+        "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f184e7c3-1912-41b2-b81f-435d1b37c7b4/1ae87107-2943-4ba6-be2b-390ca27c6260.png",
+      cardName: "uniapp",
+      cardContent: "基于vue的跨端平台。",
+    },
+    {
+      cardSrc: "https://taro.jd.com/",
+      cardImgSrc: "https://taro-ui.jd.com/img/logo-taro.png",
+      cardName: "Taro",
+      cardContent: "多端统一开发解决方案",
+    },
+    {
+      cardSrc: "https://www.reactnative.cn/docs/getting-started",
+      cardImgSrc: "https://www.reactnative.cn/img/header_logo.svg",
+      cardName: "ReactNative",
+      cardContent: "基于reactnative的跨端平台",
+    },
+    {
+      cardSrc: "https://flutter.cn/docs/get-started/install",
+      cardImgSrc: "https://flutter.cn/assets/images/cn/flutter-cn-logo.png",
+      cardName: "Flutter",
+      cardContent: "谷歌公司开发的跨端平台",
+    },
+  ],
+  // 刷题网站
+  cardData4: [
+    {
+      id: "4",
+      cardSrc: "https://leetcode.cn/problemset/all/",
+      cardImgSrc:
+        "https://static.leetcode-cn.com/cn-frontendx-assets/production/_next/static/images/lccn-logo-ce3d56eeedaae618e59e2ec5089e4834.svg",
+      cardName: "leetcode.",
+      cardContent: "程序员最大的刷题库",
+    },
+    {
+      cardSrc: "https://www.lintcode.com/problem/",
+      cardImgSrc:
+        "https://jstc.lintcode.com/lintcode-react/assets/9a007ea5cacb0eee41615be0a75c041f.svg",
+      cardName: "lintcode",
+      cardContent: "公司刷题库",
+    },
+    {
+      cardSrc: "https://www.acmcoder.com/#/practice/company",
+      cardImgSrc: "https://cdn.acmcoder.com/release/www/2.0.1/images/logo.png",
+      cardName: "赛码",
+      cardContent: "大厂在线考试刷题",
+    },
+  ],
+  // 热门社区
+  cardData5: [
+    {
+      id: "5",
+      cardSrc: "http://www.csdn.net/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220104232713.png",
+      cardName: "CDSN",
+      cardContent: "中国专业IT社区CSDN",
+    },
+    {
+      cardSrc: "http://www.cnblogs.com/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220104232039.png",
+      cardName: "博客园",
+      cardContent: "开发者的网上家园",
+    },
+    {
+      cardSrc: "https://www.oschina.net/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220104232755.png",
+      cardName: "OSChina",
+      cardContent: "中国最大的开源技术社区",
+    },
+    {
+      cardSrc: "https://segmentfault.com/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220105210328.png",
+      cardName: "饭否",
+      cardContent: "中国领先的开发者技术社区",
+    },
+    {
+      cardSrc: "https://www.jianshu.com/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220111001422.png",
+      cardName: "简书",
+      cardContent:
+        "简书是一个优质的创作社区,在这里,你可以任性地创作,一篇短文、一张照片、一首诗、一幅画……我们相信,每个人都是生活中的艺术家,有着无穷的创造力",
+    },
+    {
+      cardSrc: "https://stackoverflow.com/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220111001325.png",
+      cardName: "stack overflow",
+      cardContent:
+        "Stack Overflow是最大、最值得信赖的在线社区，供开发人员学习、分享编程知识和建立职业生涯",
+    },
+    {
+      cardSrc: "https://gitee.com/explore",
+      cardImgSrc: "https://gitee.com/static/images/logo-black.svg?t=158106664",
+      cardName: "码云",
+      cardContent:
+        "Gitee.com(码云) 是 OSCHINA.NET 推出的代码托管平台,支持 Git 和 SVN,提供免费的私有仓库托管。",
+    },
+  ],
+  // CSS 样式
+  cardData6: [
+    {
+      id: "6",
+      cardSrc: "https://less.bootcss.com/",
+      cardImgSrc: "https://cdn.docschina.org/home/logo/less.svg",
+      cardName: "Less",
+      cardContent: "Less 是 CSS 预处理语言，使 CSS 更易维护和扩展",
+    },
+    {
+      cardSrc: "https://sass.bootcss.com/guide",
+      cardImgSrc: "https://cdn.docschina.org/home/logo/sass.svg",
+      cardName: "Sass ",
+      cardContent: "Sass 是 CSS 的一个扩展，它使 CSS 的使用起来更加优雅和强大",
+    },
+    {
+      cardSrc: "https://www.stylus-lang.cn/",
+      cardImgSrc: "https://cdn.docschina.org/home/logo/stylus.svg",
+      cardName: "stylus",
+      cardContent: "富有表现力、动态、健壮的 CSS",
+    },
+  ],
+  // 可视化库
+  cardData7: [
+    {
+      id: "7",
+      cardSrc: "http://www.webgl3d.cn/Three.js/",
+      cardImgSrc: "http://www.webgl3d.cn/img/logo.png",
+      cardName: "three",
+      cardContent: "JavaScript 3d 库",
+    },
+    {
+      cardSrc: "https://www.echartsjs.com/examples/zh/index.html",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220105014053.png",
+      cardName: "Echarts",
+      cardContent: "百度开发的可定制的数据可视化图表，已经捐给Apache",
+    },
+    {
+      cardSrc: "https://antv.vision/zh",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220105014146.png",
+      cardName: "AntV",
+      cardContent:
+        "蚂蚁金服全新一代数据可视化解决方案，致力于提供一套简单方便、专业可靠、无限可能的数据可视化最佳实践",
+    },
+    {
+      cardSrc: "https://d3js.org/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220105014223.png",
+      cardName: "D3",
+      cardContent: "用动态图形显示数据的JavaScript库",
+    },
+    {
+      cardSrc: "https://www.chartjs.org/",
+      cardImgSrc:
+        "https://cdn.staticaly.com/gh/Kele-Bingtang/static@master/img/tools/20220105014412.png",
+      cardName: "ChartJs",
+      cardContent: "基于 HTML5 的 JavaScript 图表库",
+    },
+    {
+      cardSrc: "https://v-charts.js.org/#/",
+      cardImgSrc: "https://cdn.docschina.org/home/logo/vue.svg",
+      cardName: "v-charts",
+      cardContent: "基于 Vue 和 ECharts 封装的图表组件",
+    },
+    {
+      cardSrc: "http://datav.jiaminghi.com/guide/",
+      cardImgSrc:
+        "https://camo.githubusercontent.com/9a6e165b9188a8ac4e5173bc382688a418fad711/68747470733a2f2f636d6c2e6a732e6f72672f646f632f6173736574732f3130302a3130302e706e67",
+      cardName: "DataV",
+      cardContent:
+        "组件库基于Vue （React版） ，主要用于构建大屏（全屏）数据展示页面即数据可视化，具有多种类型组件可供使用",
+    },
+    {
+      cardSrc: "https://g6.antv.vision/zh/docs/manual/introduction",
+      cardImgSrc: "https://cdn.docschina.org/home/logo/jquery.svg",
+      cardName: "G6",
+      cardContent:
+        "G6 是一个图可视化引擎。它提供了图的绘制、布局、分析、交互、动画等图可视化的基础能力。",
+    },
+  ],
+  // 工具
+  cardData8: [
+    {
+      cardSrc: "https://www.ilovepdf.com/",
+      cardImgSrc: "https://www.ilovepdf.com/img/ilovepdf.svg",
+      cardName: "PDF转换",
+      cardContent: "I LOVE PDF",
+    },
+    {
+      cardSrc: "https://jsrun.net/new",
+      cardImgSrc: "https://cdns.jsrun.net/avatar/default.png",
+      cardName: "Jsrun",
+      cardContent: "JS在线编辑器",
+    },
+    {
+      cardSrc: "https://regexlearn.com/zh-cn/learn",
+      cardName: "正则",
+      cardContent: "正则练习",
+    },
+    {
+      cardSrc: "https://tool.lu/",
+      cardName: "在线工具",
+      cardContent: "在线工具",
+    },
+    {
+      cardSrc: "https://devtool.tech/",
+      cardName: "武器库",
+      cardContent: "开发者武器库",
+    },
+    {
+      cardSrc: "https://www.matools.com/",
+      cardName: "转换",
+      cardContent: "代码在线工具箱",
+    },
+    {
+      cardSrc: "https://chrome.zzzmh.cn/#/index",
+      cardName: "级简插件",
+      cardContent: "google插件",
+    },
+    {
+      cardSrc: "https://www.crxsoso.com/webstore/category/ext-7-productivity",
+      cardName: "油猴插件",
+      cardContent: "google插件2",
+    },
+    {
+      cardSrc: "https://uiverse.io/all",
+      cardImgSrc: "https://uiverse.io/build/_assets/logo-OR7QQX33.svg",
+      cardName: "按钮动画",
+      cardContent: "uiverse",
+    },
+    {
+      cardSrc: "https://www.webfx.com/tools/emoji-cheat-sheet/",
+      cardName: "md标签",
+      cardContent: "表情包",
+    },
+    {
+      cardSrc: "https://listen1.github.io/listen1/",
+      cardName: "听歌软件",
+      cardContent: "集成音乐播放器",
+    },
+    {
+      cardSrc: "https://msdn.itellyou.cn/",
+      cardName: "电脑系统",
+      cardContent: "操作系统",
+    },
+    {
+      cardSrc: "http://www.staticfile.org/",
+      cardImgSrc: "http://www.staticfile.org/assets/images/light-logo.png",
+      cardName: "静态资源",
+      cardContent: "静态资源",
+    },
+    {
+      cardSrc: "http://www.yishuzi.com/",
+      cardName: "签名",
+      cardContent: "签名生成器",
+    },
+    {
+      cardSrc: "https://www.jetbrains.com/idea/download/other.html",
+      cardName: "idea",
+      cardContent: "idea历史版本",
+    },
+    {
+      cardSrc: "https://www.exception.site/",
+      cardName: "破解",
+      cardContent: "开发软件破解码",
+    },
+    {
+      cardSrc: "https://www.bitbug.net/",
+      cardName: "制作icon图标",
+      cardContent: "制作icon图标",
+    },
+    {
+      cardSrc: "https://www.supfree.net/",
+      cardName: "在线查询",
+      cardContent: "在线查询",
+    },
+    {
+      cardSrc: "http://www.netbian.com/",
+      cardName: "桌面壁纸",
+      cardContent: "桌面壁纸",
+    },
+    {
+      cardSrc: "https://wallhaven.cc/",
+      cardName: "电脑壁纸",
+      cardContent: "电脑壁纸",
+    },
+  ],
+  // vue技术收藏
+  cardData9: [
+    {
+      cardSrc:
+        "https://www.cnblogs.com/dengsir39/archive/2022/07/11/16467487.html",
+      cardName: "记住用户名密码",
+      cardContent: "PC记住用户名密码",
+    },
+    {
+      cardSrc: "http://www.websocket-test.com/",
+      cardName: "测试websocket连接",
+      cardContent: "测试websocket连接",
+    },
+    {
+      cardSrc: "https://www.kancloud.cn/smallchill/saber/1302049",
+      cardName: "Saber文档",
+      cardContent: "Saber文档",
+    },
+    {
+      cardSrc: "https://www.iconfont.cn/",
+      cardName: "iconfont",
+      cardContent: "字体图标iconfont",
+    },
+  ],
+  // uniapp技术收藏
+  cardData10: [
+    {
+      cardSrc: "https://blog.csdn.net/qq_45978154/article/details/125886484",
+      cardName: "记住用户名密码",
+      cardContent: "uniapp 记住用户名密码",
+    },
+  ],
+  // node技术收藏
+  cardData11: [
+    {
+      cardSrc: "https://nodejs.org/zh-cn/download/releases/",
+      cardName: "node",
+      cardContent: "node历史版本",
+    },
+  ],
+  // echarts技术收藏
+  cardData12: [
+    {
+      cardSrc: "https://www.jianshu.com/p/ef2a2154ad88",
+      cardName: "大屏适配方案",
+      cardContent: "大屏适配方案",
+    },
+    {
+      cardSrc: "https://www.makeapie.cn/echarts",
+      cardName: "echarts-makeapie",
+      cardContent: "echarts-makeapie",
+    },
+    {
+      cardSrc: "https://www.isqqw.com/homepage#/echartsdetail?id=16913",
+      cardName: "echarts",
+      cardContent: "echartsDemo社区",
+    },
+    {
+      cardSrc: "http://datav.aliyun.com/portal/school/atlas/area_selector",
+      cardName: "阿里地图",
+      cardContent: "阿里地图json下载",
+    },
+    {
+      cardSrc: "https://hxkj.vip/demo/echartsMap/",
+      cardName: "地图",
+      cardContent: "地图json下载",
+    },
+  ],
+  // 设计特效
+  cardData15: [
+    {
+      cardSrc: "https://mycolor.space/?hex=%230000FF&sub=1",
+      cardName: "配色方案",
+      cardContent: "配色方案",
+    },
+    {
+      cardSrc: "https://bj.96weixin.com/tools/rgb",
+      cardName: "颜色对比",
+      cardContent: "颜色对比",
+    },
+
+    {
+      cardSrc: "http://weavesilk.com",
+      cardName: "对称光绘",
+      cardContent: "对称光绘",
+    },
+    {
+      cardSrc: "https://paveldogreat.github.io/WebGL-Fluid-Simulation",
+      cardName: "高亮流体",
+      cardContent: "高亮流体",
+    },
+    {
+      cardSrc: "https://duotone.shapefactory.co",
+      cardName: "双色生成",
+      cardContent: "双色生成",
+    },
+    {
+      cardSrc: "https://wangyasai.github.io/Stars-Emmision/",
+      cardName: "爆炸粒子",
+      cardContent: "爆炸粒子",
+    },
+    {
+      cardSrc: "https://wangyasai.github.io/Perlin-Noise/",
+      cardName: "流动的线",
+      cardContent: "流动的线",
+    },
+  ],
+  // 其他收藏
+  cardData99: [
+    {
+      cardSrc:
+        "https://blog.csdn.net/weixin_44427784/article/details/113589267",
+      cardName: "SSH步骤",
+      cardContent: "github添加SSH步骤",
+    },
+    {
+      cardSrc: "https://blog.csdn.net/qq_44922354/article/details/122047810",
+      cardName: "vuepress",
+      cardContent: "vuepress踩坑版",
+    },
+  ],
+};
